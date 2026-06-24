82b8c5938f576cef6dec824b4e30ae51c80bdc47
Lavi Sengar <lavi16.img@gmail.com>
2026-06-24 15:41:56 +0530

    Customer support changes

---
+++ apps/app_settings/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import CustomerSupport
+
+class CustomerSupportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerSupport
+        fields = ["id", "message", "created_at"]
+        read_only_fields = ["id", "created_at"]
