876f6294c094fbd428f78a02509c24f18ccee312
Lavi Sengar <lavi16.img@gmail.com>
2026-07-09 16:51:34 +0530

    changes

---
+++ apps/mileage_advisor/services/ev_factor_table.py
@@ -0,0 +1,10 @@
+# EV_FACTORS = [
+#     {"speed": 50, "fall": 1.11},
+#     {"speed": 60, "fall": 1.05},
+#     {"speed": 70, "fall": 1.00},
+#     {"speed": 80, "fall": 0.95},
+#     {"speed": 90, "fall": 0.91},
+#     {"speed": 100, "fall": 0.87},
+#     {"speed": 110, "fall": 0.83},
+#     {"speed": 120, "fall": 0.80},
+# ]
