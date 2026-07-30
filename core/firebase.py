20635113be47705c0be08a20d03dfd53448aedeb
Lavi Sengar <lavi16.img@gmail.com>
2026-07-30 17:54:47 +0530

    Firebase error handling

--- core/firebase.py
+++ core/firebase.py
@@ -1,9 +1,11 @@
+from pathlib import Path
+
 import firebase_admin
 from firebase_admin import credentials
 from django.conf import settings
 
-if not firebase_admin._apps:
-    cred = credentials.Certificate(
-        settings.FIREBASE_SERVICE_ACCOUNT_KEY_PATH
-    )
+path = Path(settings.FIREBASE_SERVICE_ACCOUNT_KEY_PATH)
+
+if path.exists() and not firebase_admin._apps:
+    cred = credentials.Certificate(str(path))
     firebase_admin.initialize_app(cred)
